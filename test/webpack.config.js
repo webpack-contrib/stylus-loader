42c1c8fe730f1dccab64a2c41b90febbd10ae1bc
Z Goddard <mzgoddard@gmail.com>
2014-05-15 11:02:59 -0700

    Test paths, stylus's node_modules behaviour and webpack resolving
    - Add test/index.html
      webpack-dev-server no longer auto generates an index.html

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,5 +1,8 @@
 // Just run "webpack-dev-server"
 module.exports = {
 	context: __dirname,
-	entry: "mocha-loader!./all.js"
+	entry: "mocha-loader!./all.js",
+	resolve: {
+		extensions: ["", ".js", ".css", ".styl"]
+	}
 };
