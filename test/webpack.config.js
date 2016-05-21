9d50ee2e36d0060db4870f91c3b6d6f8b1cec000
Daniel P. Shannon <daniel@shannon.camp>
2016-05-20 23:20:04 -0400

    Update Webpack config.

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -16,13 +16,34 @@ function includePlugin() {
   };
 }
 
-module.exports = {
-	context: __dirname,
-	entry: "mocha-loader!./all.js",
-	resolve: {
-		extensions: ["", ".js", ".css", ".styl"]
-	},
-	stylus: {
-		use: [plugin(), includePlugin()]
-	}
-};
+if (process.env.WEBPACK_VERSION === '2.1.0-beta.7') {
+  module.exports = {
+    context: __dirname,
+    entry: 'mocha-loader!./all.js',
+    resolve: {
+      enforceExtensions: false,
+      extensions: [
+        '.js',
+        '.css',
+        '.styl'
+      ]
+    },
+    stylus: {
+      use: [
+        plugin(),
+        includePlugin()
+      ]
+    }
+  };
+} else {
+  module.exports = {
+    context: __dirname,
+    entry: "mocha-loader!./all.js",
+    resolve: {
+      extensions: ["", ".js", ".css", ".styl"]
+    },
+    stylus: {
+      use: [plugin(), includePlugin()]
+    }
+  };
+}
