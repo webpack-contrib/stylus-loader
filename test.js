18f4f55aef7dd82cd116348aa1af45803a7ef75c
evilebottnawi <sheo13666q@gmail.com>
2020-09-28 17:33:44 +0300

    chore: remove unnecessary files

--- test.js
+++
@@ -1,9 +0,0 @@
-const fg = require('fast-glob');
-
-async function test() {
-  const entries = await fg(['.editorconfig', '**/index.js'], { dot: true });
-
-  return entries;
-}
-
-test().then((result) => console.log(result));
