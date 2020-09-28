45338c0319b729950d5801d3f5233a97b626c4cb
evilebottnawi <sheo13666q@gmail.com>
2020-09-28 21:21:54 +0300

    test: test

--- src/utils.js
+++ src/utils.js
@@ -336,6 +336,7 @@ async function createEvaluator(loaderContext, code, options) {
     options
   );
 
+  // eslint-disable-next-line no-console
   console.log(resolvedDependencies);
 
   return class CustomEvaluator extends Evaluator {
