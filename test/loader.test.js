7fc557c37c3a48cdbfc950723b0173fd62fa1c2a
evilebottnawi <sheo13666q@gmail.com>
2020-09-29 03:48:48 +0300

    test: resolving

--- test/loader.test.js
+++ test/loader.test.js
@@ -1054,76 +1054,61 @@ describe('loader', () => {
   });
 
   it('imports files in dir like a glob', async () => {
-    const isWin = process.platform === 'win32';
+    const rootdir = path.resolve(__dirname, 'fixtures', 'node_modules');
+    const exampleDir = path.resolve(rootdir, 'example-like-a-glob');
+    const pathDir = path.resolve(rootdir, 'like-a-glob*');
 
-    if (isWin) {
-      expect(true).toBe(true);
-    } else {
-      const rootdir = path.resolve(__dirname, 'fixtures', 'node_modules');
-      const exampleDir = path.resolve(rootdir, 'example-like-a-glob');
-      const pathDir = path.resolve(rootdir, 'like-a-glob*');
-
-      if (!fs.existsSync(pathDir)) {
-        fs.mkdirSync(pathDir);
-        fs.copyFileSync(
-          path.resolve(exampleDir, 'package.json'),
-          path.resolve(pathDir, 'package.json')
-        );
-        fs.copyFileSync(
-          path.resolve(exampleDir, 'index.styl'),
-          path.resolve(pathDir, 'index.styl')
-        );
-      }
-
-      const testId = './import-dir-like-a-glob.styl';
-      const compiler = getCompiler(testId);
-      const stats = await compile(compiler);
-      const codeFromBundle = getCodeFromBundle(stats, compiler);
-      const codeFromStylus = await getCodeFromStylus(testId);
-
-      expect(codeFromBundle.css).toBe(codeFromStylus.css);
-      expect(codeFromBundle.css).toMatchSnapshot('css');
-      expect(getWarnings(stats)).toMatchSnapshot('warnings');
-      expect(getErrors(stats)).toMatchSnapshot('errors');
+    if (!fs.existsSync(pathDir)) {
+      fs.mkdirSync(pathDir);
+      fs.copyFileSync(
+        path.resolve(exampleDir, 'package.json'),
+        path.resolve(pathDir, 'package.json')
+      );
+      fs.copyFileSync(
+        path.resolve(exampleDir, 'index.styl'),
+        path.resolve(pathDir, 'index.styl')
+      );
     }
+
+    const testId = './import-dir-like-a-glob.styl';
+    const compiler = getCompiler(testId);
+    const stats = await compile(compiler);
+    const codeFromBundle = getCodeFromBundle(stats, compiler);
+    const codeFromStylus = await getCodeFromStylus(testId);
+
+    expect(codeFromBundle.css).toBe(codeFromStylus.css);
+    expect(codeFromBundle.css).toMatchSnapshot('css');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
   });
 
   it('imports files in dir like a glob through webpack', async () => {
-    const isWin = process.platform === 'win32';
+    const rootdir = path.resolve(__dirname, 'fixtures', 'node_modules');
+    const exampleDir = path.resolve(rootdir, 'example-like-a-glob');
+    const pathDir = path.resolve(rootdir, 'webpack-like-a-glob-package-name*');
 
-    if (isWin) {
-      expect(true).toBe(true);
-    } else {
-      const rootdir = path.resolve(__dirname, 'fixtures', 'node_modules');
-      const exampleDir = path.resolve(rootdir, 'example-like-a-glob');
-      const pathDir = path.resolve(
-        rootdir,
-        'webpack-like-a-glob-package-name*'
+    if (!fs.existsSync(pathDir)) {
+      fs.mkdirSync(pathDir);
+      fs.copyFileSync(
+        path.resolve(exampleDir, 'package.json'),
+        path.resolve(pathDir, 'package.json')
       );
-
-      if (!fs.existsSync(pathDir)) {
-        fs.mkdirSync(pathDir);
-        fs.copyFileSync(
-          path.resolve(exampleDir, 'package.json'),
-          path.resolve(pathDir, 'package.json')
-        );
-        fs.copyFileSync(
-          path.resolve(exampleDir, 'index.styl'),
-          path.resolve(pathDir, 'index.styl')
-        );
-      }
-
-      const testId = './import-webpack-dir-like-a-glob.styl';
-      const compiler = getCompiler(testId);
-      const stats = await compile(compiler);
-      const codeFromBundle = getCodeFromBundle(stats, compiler);
-      const codeFromStylus = await getCodeFromStylus(testId);
-
-      expect(codeFromBundle.css).toBe(codeFromStylus.css);
-      expect(codeFromBundle.css).toMatchSnapshot('css');
-      expect(getWarnings(stats)).toMatchSnapshot('warnings');
-      expect(getErrors(stats)).toMatchSnapshot('errors');
+      fs.copyFileSync(
+        path.resolve(exampleDir, 'index.styl'),
+        path.resolve(pathDir, 'index.styl')
+      );
     }
+
+    const testId = './import-webpack-dir-like-a-glob.styl';
+    const compiler = getCompiler(testId);
+    const stats = await compile(compiler);
+    const codeFromBundle = getCodeFromBundle(stats, compiler);
+    const codeFromStylus = await getCodeFromStylus(testId);
+
+    expect(codeFromBundle.css).toBe(codeFromStylus.css);
+    expect(codeFromBundle.css).toMatchSnapshot('css');
+    expect(getWarnings(stats)).toMatchSnapshot('warnings');
+    expect(getErrors(stats)).toMatchSnapshot('errors');
   });
 
   it('imports files listed in option as glob', async () => {
